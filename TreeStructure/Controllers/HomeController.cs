963367ea97999c34531ea432caff11b21be4e535
Asamarioth <darkstorm203@gmail.com>
2022-04-14 15:45:59 +0200

    Add project files.

---
+++ TreeStructure/Controllers/HomeController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TreeStructure.Models;
+
+namespace TreeStructure.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDb _appDb;
+        private TreeNodeRepository _treeRepository;
+
+        public HomeController(AppDb db)
+        {
+            _appDb = db;
+            _treeRepository = new TreeNodeRepository(db);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> InsertNode(TreeNode node)
+        {
+            try
+            {
+                if (node.ParentId.HasValue)
+            {
+                if (_treeRepository.FindOneAsync(node.ParentId.Value) is null)
+                {
+                    return BadRequest("Parent does not exist");
+                }
+            }
+ 
+                await _treeRepository.InsertOneAsync(node);
+                return Ok("Node has been created");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteNode(TreeNode node)
+        {
+            try
+            {
+                if (_treeRepository.FindOneAsync(node.Id) is null)
+                {
+                    return BadRequest("Node not found");
+                }
+                await _treeRepository.RemoveNodeRecursively(node.Id);
+                return Ok("Node has been deleted");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPatch]
+        public async Task<IActionResult> MoveNode(TreeNode child, TreeNode? newParent)
+        {
+            try
+            {
+                if (_treeRepository.FindOneAsync(child.Id) is null)
+            {
+                return BadRequest("Node does not exist");
+            }
+            if(newParent is null)
+            {
+                await _treeRepository.ChangeParentAsync(child.Id, null);
+                return Ok();
+
+            }
+            if(_treeRepository.FindOneAsync(newParent.Id) is null)
+            {
+                return BadRequest("Parent does not exist");
+            }
+
+               await _treeRepository.ChangeParentAsync(child.Id, newParent.Id);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPatch]
+        public async Task<IActionResult> UpdateNode(TreeNode node)
+        {
+            try
+            {
+                if(_treeRepository.FindOneAsync(node.Id) is null)
+                {
+                    return BadRequest("Node does not exist");
+                }
+                await _treeRepository.UpdateNameAsync(node);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TreeStructure/Repositories/TreeNodeRepository.cs
@@ -0,0 +1,95 @@
+﻿using MySqlConnector;
+using System.Data.Common;
+using TreeStructure.Models;
+
+namespace TreeStructure
+{
+    public class TreeNodeRepository
+    {
+        public AppDb Db { get; }
+
+        public TreeNodeRepository(AppDb db)
+        {
+            Db = db;
+        }
+        public async Task InsertOneAsync(TreeNode node)
+        {
+           await node.InsertAsync();
+        }
+        public async Task<TreeNode?> FindOneAsync(int id)
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"SELECT `Id`, `ParentId`, `Name` FROM `tree` WHERE `Id` = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            var result = await ReadAllAsync(await cmd.ExecuteReaderAsync());
+            return result.Count > 0 ? result[0] : null;
+        }
+
+        public async Task<List<TreeNode>?> GetChildrenAsync(int parentId)
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"SELECT `Id`, `ParentId`, `Name` FROM `tree` WHERE `ParentId` = @parentId";
+            cmd.Parameters.AddWithValue("@parentId", parentId);
+            var result = await ReadAllAsync(await cmd.ExecuteReaderAsync());
+            return result.Count > 0 ? result : null;
+        }
+        public async Task RemoveNodeRecursively(int id)
+        {
+            var children = await GetChildrenAsync(id);
+
+            if (children is not null)
+            {
+                foreach (var child in children)
+                {
+                    await RemoveNodeRecursively(child.Id);
+                }
+            }
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"DELETE FROM `tree` WHERE `Id` = @id;";
+            cmd.Parameters.AddWithValue("@id", id);
+            await cmd.ExecuteNonQueryAsync();
+        }
+        public async Task ChangeParentAsync(int id, int? newParentId)
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            if (newParentId.HasValue)
+            {
+                cmd.CommandText = @"UPDATE `tree` SET `ParentId` = @parentId WHERE `Id` = @id;";
+                cmd.Parameters.AddWithValue("@parentId", newParentId);
+                cmd.Parameters.AddWithValue("@id", id);
+            }
+            else
+            {
+                cmd.CommandText = @"UPDATE `tree` SET `ParentId` = NULL WHERE `Id` = @id;";
+                cmd.Parameters.AddWithValue("@id", id);
+            }
+            await cmd.ExecuteNonQueryAsync();
+
+        }
+        public async Task UpdateNameAsync(TreeNode node)
+        {
+            await node.UpdateNameAsync();
+        }
+        public async Task<List<TreeNode>> ReadAllAsync(DbDataReader reader)
+        {
+            var nodes = new List<TreeNode>();
+            using (reader)
+            {
+                while (await reader.ReadAsync())
+                {
+                    var node = new TreeNode(Db)
+                    {
+                        Id = reader.GetInt32(0),
+                        ParentId = reader.GetInt32(1),
+                        Name = reader.GetString(2),
+                    };
+                    nodes.Add(node);
+                }
+            }
+            return nodes;
+        }
+        
+
+    }
+
+}
